aa28bb8927380aa494a82db29056de1010b7a0ce
deboraptor <vdzdebora@gmail.com>
2024-03-15 12:41:00 +0100

    modifications par ordre croissant

--- extraire_eaf.py
+++ extraire_eaf.py
@@ -3,13 +3,19 @@ from bs4 import BeautifulSoup as bs
 with open("../M1/11870482.001.eaf", "r") as f:
     soup = bs(f, features="xml")
     alignable_annotation = soup.find_all('ALIGNABLE_ANNOTATION')
-    # print(annotation_value)
-    text = ""
+    annotation_value_liste = []
     for balise in alignable_annotation:
+        time_slot_ref1 = balise.get('TIME_SLOT_REF1')
         annotation_value = balise.find('ANNOTATION_VALUE')
-        if annotation_value:
-            text += annotation_value.text + "\n"
-            with open("resultats.csv", 'w') as file:
-                file.write(text)
 
+        # Je récupère dans la balise annotation_value la valeur de time_slot_ref1
+        # Je la met dans un dictionnaire dans l'ordre d'apparition
+        if time_slot_ref1 and annotation_value:
+            annotation_value_liste.append((time_slot_ref1, annotation_value.text.strip()))
 
+# Et là je trie les balises des annotations par ordre croissant par rapport au time_slot_ref1
+sorted_annotation_values = sorted(annotation_value_liste, key=lambda x: x[0])
+
+with open("resultats.csv", 'w') as file:
+    for time_slot, annotation_value in sorted_annotation_values:
+        file.write(annotation_value + "\n")
