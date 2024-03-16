7a084923f87cf3a63236d10784062782514b94b1
deboraptor <vdzdebora@gmail.com>
2024-03-16 22:13:16 +0100

    ajout des noms des participants

--- extraire_eaf.py
+++ extraire_eaf.py
@@ -1,7 +1,22 @@
 from bs4 import BeautifulSoup as bs
+import csv 
 
-with open("../M1/11870482.001.eaf", "r") as f:
-    soup = bs(f, features="xml")
+with open("../M1/11870482.001.eaf", "r") as file:
+    soup = bs(file, features="xml")
+
+    # Je récupère les id des participants dans tier
+    tier = soup.find_all('TIER')
+    participant_liste = []
+    for balise in tier:
+        participant = balise.get('TIER_ID')
+        if participant == "*PRE":
+            participant_liste.append('Présentateur')        
+        else:
+            if participant == None:
+                participant_liste.append('Inconnu')
+            else:
+                participant_liste.append(participant)
+    print(participant_liste)
     alignable_annotation = soup.find_all('ALIGNABLE_ANNOTATION')
     annotation_value_liste = []
     for balise in alignable_annotation:
@@ -16,6 +31,15 @@ with open("../M1/11870482.001.eaf", "r") as f:
 # Et là je trie les balises des annotations par ordre croissant par rapport au time_slot_ref1
 sorted_annotation_values = sorted(annotation_value_liste, key=lambda x: x[0])
 
-with open("resultats.csv", 'w') as file:
-    for time_slot, annotation_value in sorted_annotation_values:
-        file.write(annotation_value + "\n")
+
+file = open("resultats.csv", 'w')
+
+# Je fais une liste de compréhension pour que chaque élément soit une colonne pour le format csv
+data = [(participant, annotation_value) for time_slot, annotation_value in sorted_annotation_values]
+
+# Il faut maintenant mettre ligne par ligne chaque élément de la liste
+objet = csv.writer(file)
+for element in data:
+    objet.writerow(element)
+
+file.close()
