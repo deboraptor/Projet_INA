290f74cb7a43a8696a68647d9d901efdadf574bb
Maoui <maoui@MacBook-Pro-de-Maoui.local>
2024-04-05 10:37:44 +0200

    test nltk

--- polarity_nltk_test.py
+++ polarity_nltk_test.py
@@ -3,7 +3,12 @@ VADER is best suited for language used in social media, like short sentences wit
 It’s less accurate when rating longer, structured sentences, but it’s often a good launching point.
 """
 
+import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
+
+exemple = "nul !"
 sia = SentimentIntensityAnalyzer()
-sia.polarity_scores("Wow, NLTK is really powerful!")
-{'neg': 0.0, 'neu': 0.295, 'pos': 0.705, 'compound': 0.8012}
+
+sentiment_score = sia.polarity_scores(exemple)
+print(exemple)
+print(sentiment_score)
