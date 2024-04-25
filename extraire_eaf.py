9072fa7f48234d8c8fd29e9430827362341c3840
deboraptor <vdzdebora@gmail.com>
2024-04-25 20:48:15 +0200

    feat: j'essaye d'affficher le nom de la personne qui parle mais je n'y arrive pas

--- extraire_eaf.py
+++ extraire_eaf.py
@@ -1,23 +1,29 @@
 from bs4 import BeautifulSoup as bs
-import csv 
+import csv
+import glob
+import os
 
-with open("../M1/11870482.001.eaf", "r") as file:
-    soup = bs(file, features="xml")
+dossier_fichiers = glob.glob("../M1/*.eaf")
+for fichier in dossier_fichiers:
+    with open(fichier, "r") as file:
+        soup = bs(file, features="xml")
 
-    # Je récupère les id des participants dans tier
+    nom_fichier_csv = os.path.basename(fichier).replace(".eaf", ".csv")
+
+    ## Je récupère les id des participants dans tier
     tier = soup.find_all('TIER')
     participant_liste = []
+    participant_dict = {}
     for balise in tier:
         participant = balise.get('TIER_ID')
+        time_slot = balise.find('TIME_SLOT')
+        if time_slot:
+            time_slot_num = int(time_slot.get('TIME_SLOT_ID').split('ts')[-1])
+            participant_dict[time_slot_num] = participant
         if participant == "*PRE":
-            participant_liste.append('Présentateur')        
-        else:
-            if participant == None:
-                participant_liste.append('Inconnu')
-            else:
-                participant_liste.append(participant)
+            participant_liste.append('Présentateur')
 
-    # Les annotations
+    ## Les annotations
     alignable_annotation = soup.find_all('ALIGNABLE_ANNOTATION')
     annotation_value_liste = []
     for balise in alignable_annotation:
@@ -27,25 +33,15 @@ with open("../M1/11870482.001.eaf", "r") as file:
         # Je récupère dans la balise annotation_value la valeur de time_slot_ref1
         # Je la met dans un dictionnaire dans l'ordre d'apparition
         if time_slot_ref1 and annotation_value:
-            annotation_value_liste.append((time_slot_ref1, annotation_value.text.strip()))
+            time_slot_num = int(time_slot_ref1.split('ts')[-1])
+            participant = participant_dict.get(time_slot_num, 'Inconnu')
+            annotation_value_liste.append((participant, time_slot_ref1, annotation_value.text.strip()))
 
-# Et là je trie les balises des annotations par ordre croissant par rapport au time_slot_ref1
-sorted_annotation_values = sorted(annotation_value_liste, key=lambda x: x[0])
+    # Et là je trie les balises des annotations par ordre croissant par rapport au time_slot_ref1
+    sorted_annotation_values = sorted(annotation_value_liste, key=lambda x: int(x[1].split('ts')[-1]))
 
-"""
-file = open("resultats.csv", 'w')
-
-# Je fais une liste de compréhension pour que chaque élément soit une colonne pour le format csv
-data = [(participant, annotation_value) for time_slot, annotation_value in sorted_annotation_values]
-
-# Il faut maintenant mettre ligne par ligne chaque élément de la liste
-objet = csv.writer(file)
-for element in annotation_value_liste:
-    # objet.writerow(element)
-    file.write(element)
-file.close()
-"""
-
-with open("resultats.csv", 'w') as file:
-    for time_slot, annotation_value in sorted_annotation_values:
-        file.write(annotation_value + "\n")
+    # Écrit les annotations avec les noms des locuteurs dans le fichier CSV
+    with open(os.path.join('fichiers_extraits', nom_fichier_csv), 'w', newline='') as csvfile:
+        objet = csv.writer(csvfile)
+        for participant, _, annotation_value in sorted_annotation_values:
+            objet.writerow([participant, annotation_value])
