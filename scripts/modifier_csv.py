b1a7ae5c56e84fe968e02458bb4bec6fe096367c
deboraptor <vdzdebora@gmail.com>
2024-04-27 00:02:39 +0200

    add: ajout d'un dossier pour ranger les scripts dedans

---
+++ scripts/modifier_csv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import glob
+import os
+
+
+fichiers = glob.glob("./fichiers_extraits/*.csv") 
+
+for fichier in fichiers:
+    df = pd.read_csv(fichier, sep="\t")
+    # print("DEBUT", df.columns.values)
+    df = df.drop(df.columns[[1, 2, 3, 4]], axis=1)
+    # print("RESULTAT", df.columns.values)
+    base = os.path.splitext(fichier)[0]
+    df.to_csv(f"{base}.csv", index=False, header=False)
