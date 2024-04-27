60b2c580e50701a8a82524f027c555878ad0c47c
deboraptor <vdzdebora@gmail.com>
2024-04-27 04:10:10 +0200

    feat: ajout d'une indication pour les succès et les erreurs

--- scripts/csv2txt.py
+++ scripts/csv2txt.py
@@ -2,12 +2,24 @@ import glob
 
 fichiers = glob.glob("../data/fichiers_csv/*.csv")
 
+succes = 0
+erreur = 0
+
 for fichier in fichiers:
-    csv2txt = fichier.replace("../data/fichiers_csv/", "../data/fichiers_txt/").replace(".csv", ".txt")
-    with open(fichier, 'r') as csv:
-        lignes = csv.readlines()
-        with open(csv2txt, "w") as txt:
-            for ligne in lignes:
-                if ',' in ligne:
-                    partie_droite = ligne.split(',', 1)[1].strip()
-                    txt.write(partie_droite + '\n')
+    try:
+        csv2txt = fichier.replace("../data/fichiers_csv/", "../data/fichiers_txt/").replace(".csv", ".txt")
+        with open(fichier, 'r') as csv:
+            lignes = csv.readlines()
+            with open(csv2txt, "w") as txt:
+                for ligne in lignes:
+                    if ',' in ligne:
+                        partie_droite = ligne.split(',', 1)[1].strip()
+                        txt.write(partie_droite + '\n')
+        succes += 1
+    except Exception as e:
+        erreur += 1
+        print(f"Erreur lors de la conversion '{fichier}': {str(e)}")
+
+print(f"\n{succes} fichiers convertis avec succès.")
+if erreur > 0:
+    print(f"{erreur} fichiers n'ont pas été convertis à cause d'une erreur.")
