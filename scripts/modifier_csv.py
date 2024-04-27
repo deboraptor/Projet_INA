ca743866b5065c0d1c557b00aa9bfe5f13c03f53
deboraptor <vdzdebora@gmail.com>
2024-04-27 04:09:21 +0200

    feat: ajout d'une indication d'erreur ou de succès

--- scripts/modifier_csv.py
+++ scripts/modifier_csv.py
@@ -2,13 +2,22 @@ import pandas as pd
 import glob
 import os
 
+fichiers = glob.glob("./data/fichiers_csv_/*.csv")
 
-fichiers = glob.glob("./data/fichiers_csv_/*.csv") 
+succes = 0
+erreur = 0
 
 for fichier in fichiers:
-    df = pd.read_csv(fichier, sep="\t")
-    # print("DEBUT", df.columns.values)
-    df = df.drop(df.columns[[1, 2, 3, 4]], axis=1)
-    # print("RESULTAT", df.columns.values)
-    base = os.path.splitext(fichier)[0]
-    df.to_csv(f"{base}.csv", index=False, header=False)
+    try:
+        df = pd.read_csv(fichier, sep="\t")
+        df = df.drop(df.columns[[1, 2, 3, 4]], axis=1)
+        base = os.path.splitext(fichier)[0]
+        df.to_csv(f"{base}.csv", index=False, header=False)
+        succes += 1
+    except Exception as e:
+        erreur += 1
+        print(f"Erreur lors de l'actualisation du fichier CSV '{fichier}': {str(e)}")
+
+print(f"\n{succes} fichiers actualisés avec succès.")
+if erreur > 0:
+    print(f"{erreur} fichiers n'ont pas été actualisés à cause d'une erreur.")
