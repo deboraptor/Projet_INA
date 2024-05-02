42e706038cf63d0fbc94488042b0c3c964542175
deboraptor <vdzdebora@gmail.com>
2024-05-03 01:07:45 +0200

    feat: changements des métriques

--- emotions/metriques.py
+++ emotions/metriques.py
@@ -2,8 +2,12 @@ from compter_sentiments import machine, gold
 
 import pandas as pd
 import tabulate
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 
 
@@ -22,6 +26,17 @@ if len(machine) == len(gold):
     metrique = [['Précision', str(precision)], ['Rappel', str(recall)], ['F-mesure', str(f1_score)]]
     table = tabulate.tabulate(metrique, headers=['Métrique', 'Valeur'], tablefmt="rounded_outline")
     print(table)
+
+    conf_matrix = confusion_matrix(gold, machine)
+    classes = np.unique(np.concatenate((gold, machine)))
+    
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(conf_matrix, annot=True, cmap='Blues', fmt='g', xticklabels=classes, yticklabels=classes)
+
+    plt.xlabel('Classe Prédite')
+    plt.ylabel('Classe Réelle')
+    plt.title('Matrice de Confusion')
+    plt.savefig("./output/graphes/matrice_confusion.png")
 
 else:
     print("Les listes ne sont pas de même taille.")
